5074d617f94eee4f577533e882a7873f25d2b850
ethan.lam <ethan.lam@kwehk.local>
2019-02-13 13:45:12 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,80 @@
+var express = require('express')
+var session = require('cookie-session')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http')
+var url = require('url')
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert')
+var ObjectId = require('mongodb').ObjectID
+var mongourl = 'mongodb://hellowah5:hellowah5@ds149682.mlab.com:49682/11664934'
+var mongoose = require('mongoose')
+var fs = require('fs')
+var formidable = require('formidable')
+var util = require('util')
+var multer = require('multer')
+var upload = multer()
+
+app = express()
+
+app.set('view engine', 'ejs')
+
+var SECRETKEY1 = 'I GO TO BY BUS'
+var SECRETKEY2 = 'XYZ'
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+	res.status(200)
+	res.render('postForm')
+})
+
+app.post('/form', upload.array(), (req, res) => {
+	let formData = req.body;
+	console.log('form data', formData);
+	let output = []
+
+	if( isArray(formData.Title) ) {
+		for (var i=0; i < formData.Title.length; i++) {
+			var object = new Object()
+			object.Title = formData.Title[i]
+			object.Lat = formData.Lat[i]
+			object.Lng = formData.Lng[i]
+			object.Description = formData.Description[i]
+			output.push(object)
+		}
+	} else {
+		var object = new Object()
+			object.Title = formData.Title
+			object.Lat = formData.Lat
+			object.Lng = formData.Lng
+			object.Description = formData.Description
+			output.push(object)
+	}
+	console.log(output)
+
+	MongoClient.connect(mongourl, function (err, db) {
+		if (err) throw err
+
+		db.collection("newRoute").insert(output, function (err, res) {
+			if (err) throw err
+			console.log("1 document inserted")
+		})
+
+	})
+	
+	res.writeHead(200, { "Content-Type": "text/html" })
+	res.write('<html><body>')
+	res.write('<h1>Insert Success!</h1>')
+	res.end('</body></html>')
+});
+
+function isArray(obj){
+    return !!obj && obj.constructor === Array;
+}
+
+app.listen(process.env.PORT || 8099)
---
+++ server_mongoDB.js
@@ -0,0 +1,66 @@
+var http = require('http');
+var url  = require('url');
+var MongoClient = require('mongodb').MongoClient; 
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var mongourl = 'mongodb://project:project1234@ds245532.mlab.com:45532/chung_marco';
+
+var server = http.createServer(function (req,res) {
+	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+
+	var parsedURL = url.parse(req.url,true); //true to get query as object
+
+	switch(parsedURL.pathname) {
+		case '/read':
+			read_n_print(res);
+			break;
+		case '/create':
+		case '/update':
+		case '/delete':
+			res.writeHead(500, {"Content-Type": "text/plain"});
+			res.write(parsedURL.pathname + " not available yet\n");
+			res.end();
+			break;
+		default:
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.write("404 Not Found\n");
+			res.end();
+	}
+});
+
+function read_n_print(res) {
+	MongoClient.connect(mongourl, function(err, db) {
+		assert.equal(err,null);
+		console.log('Connected to MongoDB\n');
+		findRestaurants(db,function(restaurants) {
+			db.close();
+			console.log('Disconnected MongoDB\n');
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.write('<html><head><title>Restaurant</title></head>');
+			res.write('<body><H1>Restaurants</H1>');
+			res.write('<H2>Showing '+restaurants.length+' document(s)</H2>');
+			res.write('<ol>');
+			for (var i in restaurants) {
+				res.write('<li>'+restaurants[i].name+'</li>');
+			}
+			res.write('</ol>');
+			res.end('</body></html>');
+			return(restaurants);
+		}); 
+	});
+}
+
+function findRestaurants(db,callback) {
+	var restaurants = [];
+	cursor = db.collection('restaurants').find().limit(20); 
+	cursor.each(function(err, doc) {
+		assert.equal(err, null); 
+		if (doc != null) {
+			restaurants.push(doc);
+		} else {
+			callback(restaurants); 
+		}
+	});
+}
+
+server.listen(process.env.PORT || 8099);
